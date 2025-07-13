08223f17f34898b6a222f40e1a1b0321cf69e21d
Shouvik Sinha <singhashouvik616@gmail.com>
2025-07-13 21:30:10 +0530

    Initial code added.

---
+++ sinha.java
@@ -0,0 +1,28 @@
+interface Car {
+    default void engine() {
+        System.out.println("Vooo..oooo");
+    }
+
+    void music();
+
+    void airBag();
+}
+
+class Mahindra implements Car {
+    public void music(){
+        System.out.println("Playing music in Mahindra");
+    }
+    public void airBag() {
+        System.out.println("Airbag deployed in Mahindra");
+    }
+}
+
+class sinha {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Mahindra mahindra = new Mahindra();
+        mahindra.engine();
+        mahindra.music();
+        mahindra.airBag();
+    }
+}
