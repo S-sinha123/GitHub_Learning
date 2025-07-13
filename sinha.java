0d4c2af903f28a7af7c3c410ccda9d7a24847528
Shouvik Sinha <singhashouvik616@gmail.com>
2025-07-13 21:54:40 +0530

    Add java8 Features.

--- sinha.java
+++ sinha.java
@@ -1,3 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 interface Car {
     default void engine() {
         System.out.println("Vooo..oooo");
@@ -9,9 +15,10 @@ interface Car {
 }
 
 class Mahindra implements Car {
-    public void music(){
+    public void music() {
         System.out.println("Playing music in Mahindra");
     }
+
     public void airBag() {
         System.out.println("Airbag deployed in Mahindra");
     }
@@ -25,5 +32,28 @@ class sinha {
         mahindra.music();
         mahindra.airBag();
         System.out.println("This is a Mahindra car.");
+        List<Integer> array = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        // Predicate<Integer>p = new Predicate<Integer>() {
+        //     public boolean test(Integer num) {
+        //         if(num % 2 == 0){
+        //             return true;
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     }
+            
+        // };
+        // Function<Integer,Integer>f = new Function<Integer,Integer>() {
+        //     public Integer apply(Integer num){
+        //         return num*2;
+        //     }
+        // };
+        // Consumer<Integer>c = new Consumer<Integer>() {
+        //     public void accept(Integer num){
+        //         System.out.println(num);
+        //     }
+        // };
+        array.stream().filter(p->p%2==0).map(f->f*2).forEach(n->System.out.println(n));
     }
 }
